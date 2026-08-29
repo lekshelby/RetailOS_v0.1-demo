6a51a1d949d97ddd55be37b95abe92af3bfbf7cc
lekshelby <lekshelby@gmail.com>
2026-08-29 14:21:22 +0800

    Update checkout.module.ts

--- apps/api/src/checkout/checkout.module.ts
+++ apps/api/src/checkout/checkout.module.ts
@@ -3,5 +3,6 @@ import { DatabaseModule } from '../database/database.module';
 import { CheckoutController } from './checkout.controller';
 import { CheckoutService } from './checkout.service';
 import { ThermalPrinterService } from './thermal-printer.service';
-@Module({ imports: [DatabaseModule], controllers: [CheckoutController], providers: [CheckoutService, ThermalPrinterService] })
+@Module({ imports: [DatabaseModule], controllers: [CheckoutController], providers: [CheckoutService, ThermalPrinterService], exports: [ThermalPrinterService] })
 export class CheckoutModule {}
+
