9b691eb5087d638223ec78eb0216766a2e474570
lekshelby <lekshelby@gmail.com>
2026-08-29 15:25:33 +0800

    Update management.controller.ts

--- apps/api/src/management/management.controller.ts
+++ apps/api/src/management/management.controller.ts
@@ -1,5 +1,5 @@
 import { Body, Controller, Get, Param, Post, Put, Query } from '@nestjs/common';
-import { CreateManagedContactDto, CreateManagedProductDto, ManagerRequestDto, UpdateCompanyProfileDto, UpdateManagedProductDto } from './dto/management.dto';
+import { CreateManagedContactDto, CreateManagedProductDto, CreateManagedStaffDto, ManagerRequestDto, UpdateCompanyProfileDto, UpdateManagedProductDto, UpdateManagedStaffDto } from './dto/management.dto';
 import { ManagementService } from './management.service';
 
 @Controller('management')
@@ -7,6 +7,9 @@ export class ManagementController {
   constructor(private readonly management: ManagementService) {}
   @Get('company') company(@Query() input: ManagerRequestDto) { return this.management.profile(input); }
   @Put('company') updateCompany(@Body() input: UpdateCompanyProfileDto) { return this.management.updateProfile(input); }
+  @Get('staff') staff(@Query() input: ManagerRequestDto) { return this.management.listStaff(input); }
+  @Post('staff') createStaff(@Body() input: CreateManagedStaffDto) { return this.management.createStaff(input); }
+  @Put('staff/:id') updateStaff(@Param('id') id: string, @Body() input: UpdateManagedStaffDto) { return this.management.updateStaff(id, input); }
   @Get('products') products(@Query() input: ManagerRequestDto, @Query('query') query?: string) { return this.management.listProducts(input, query); }
   @Get('products/:id') product(@Param('id') id: string, @Query() input: ManagerRequestDto) { return this.management.product(id, input); }
   @Post('products') createProduct(@Body() input: CreateManagedProductDto) { return this.management.createProduct(input); }
